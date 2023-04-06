19eafacc0a50a31394f3c5e54ddabda653c12d5c
Krishnan RA <krishnanra.92@gmailc.om>
2023-04-07 00:09:15 +0530

    After Blogs

--- src/app/page/blogs/blogdetail/blogdetail.component.ts
+++ src/app/page/blogs/blogdetail/blogdetail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Blog } from 'src/app/modal/blog';
+import { BlogsService } from 'src/services/blogs.service';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-blogdetail',
   templateUrl: './blogdetail.component.html',
-  styleUrls: ['./blogdetail.component.css']
+  styleUrls: ['./blogdetail.component.css'],
 })
 export class BlogdetailComponent implements OnInit {
+  blogid: any;
+  blog: any;
+  content: string = '';
+  constructor(
+    private route: ActivatedRoute,
+    private _blogsService: BlogsService,
+    private sanitizer: DomSanitizer
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    debugger;
+    this.blogid = this.route.snapshot.paramMap.get('blogid');
+    this.getBlog();
   }
-
+  getBlog() {
+    this._blogsService
+      .getBlogDetail(this.blogid)
+      .subscribe((response: Blog) => {
+        this.content = response.content as string;
+        this.blog = this.sanitizer.bypassSecurityTrustHtml(this.content);
+      });
+  }
 }
---
+++ src/services/blogs.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpEvent,
+  HttpHeaders,
+  HttpParams,
+  HttpResponse,
+} from '@angular/common/http';
+import { Recipe } from 'src/app/modal/recipe';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Blog } from 'src/app/modal/blog';
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogsService {
+  constructor(private http: HttpClient) {}
+  public getBlogDetail(blogid: any): Observable<Blog> {
+    debugger;
+    const url = environment.apiUrl + 'Blog/GetBlogDetail';
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append('blogid', blogid);
+    return this.http.get<Blog>(url, { params: queryParams });
+  }
+  public getBlogList(): Observable<Blog[]> {
+    const url = environment.apiUrl + 'Blog/GetBlogList';
+    return this.http.get<Blog[]>(url);
+  }
+}
